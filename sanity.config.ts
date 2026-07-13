df63081e4c2100ba4f3b4ca8c38db85ef1b731e1
theredditrepreneur <theredditrepreneur@gmail.com>
2026-07-13 02:02:03 +0100

    Synchronise Studio routing and Weekly slugs

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,4 +4,4 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './sanity/schema'
 import {apiVersion,dataset,projectId} from './sanity/env'
-export default defineConfig({name:'redditrepreneur',title:'The Redditrepreneur Research',projectId,dataset,plugins:[structureTool(),visionTool({defaultApiVersion:apiVersion})],schema:{types:schemaTypes}})
+export default defineConfig({name:'redditrepreneur',title:'The Redditrepreneur Research',basePath:'/studio',projectId,dataset,plugins:[structureTool(),visionTool({defaultApiVersion:apiVersion})],schema:{types:schemaTypes}})
